7e02fdb28e562a12467c73ee73d7246e690c9930
Czupryn013 <piotrekcz09@o2.pl>
2022-02-13 15:05:07 +0100

    commit 2

---
+++ src/main/java/czupryn/pl/Bob/BobApplication.java
@@ -0,0 +1,13 @@
+package czupryn.pl.Bob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobApplication.class, args);
+	}
+
+}
